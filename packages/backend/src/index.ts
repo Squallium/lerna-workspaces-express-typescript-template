6cffbab488cd1f0a8b819e288bf01844ef656eb5
Borja Refoyo Ruiz <squallium3@gmail.com>
2023-07-23 08:55:51 +0200

    feat(core): added webpack to build and nodemon for dev

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,49 @@
-console.log('Hello backend!')
+import {Express} from "express";
+import {IExpressApp} from "@squallium-template/backend-common/src/interfaces";
+import {StartUp} from "@squallium-template/backend-common/src";
+import { config, loadConfig } from '@app-config/main';
+import { dirname, resolve as resolvePath } from 'path';
+import { ItemRoutes } from "@squallium-template/plugin-todo-backend/src"
+
+// import routes
+// import services
+// Lazy Begin Imports
+// Lazy End Imports
+
+export class CoreTestMicroServiceApp implements IExpressApp {
+
+    getSyncProcess(): any[] {
+
+        // register connections of the microservice
+        // new MigrationService().registerConnection('placeholder', placeholderConn);
+
+        // array of promises to be completed before run the application
+        return [
+            // loading app-config synchronously
+            loadConfig({
+                directory: dirname(resolvePath(__dirname, '../../')),
+            })
+            // Lazy Begin Promises
+            // Lazy End Promises
+        ]
+    }
+
+    setRoutes(app: Express): void {
+        // app.use('/<placeholder>', PlaceholderRoutes);
+        app.use('/items', ItemRoutes);
+    }
+
+    getAdminBroResources(): any[] {
+        return [
+            // Lazy Begin Bro
+            // Lazy End Bro
+        ]
+    }
+}
+
+/**
+ * Initialize backend sequence
+ */
+
+const startUp: StartUp = new StartUp(new CoreTestMicroServiceApp());
+startUp.startUpSequence();
---
+++ packages/backend/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const pathResolve = pathEntry => path.resolve(__dirname, pathEntry);
+const nodeExternals = require('webpack-node-externals');
+const NodemonPlugin = require('nodemon-webpack-plugin'); // Ding
+
+const {
+    NODE_ENV = 'production',
+} = process.env;
+module.exports = {
+    entry: './src/index.ts',
+    mode: NODE_ENV,
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    'ts-loader',
+                ]
+            }
+        ]
+    },
+    externals: [
+        nodeExternals({
+            allowlist: [/^@squallium-template/],
+            modulesFromFile: true
+        })
+    ],
+    watch: NODE_ENV === 'development',
+    plugins: [
+        new NodemonPlugin(),
+    ],
+}
